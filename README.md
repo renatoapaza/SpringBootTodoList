55057f3842b02179c8232e9580eb1112922a7846
Renato Apaza <rapaza@ypfb.gob.bo>
2018-11-21 10:02:16 -0400

    proyecto inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todo List
+
+Ejemlo Sring Boot Web MVC con groovy & postgres
---
+++ src/main/groovy/com/rapaza/todolist/TodoListApplication.groovy
@@ -0,0 +1,11 @@
+package com.rapaza.todolist
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TodoListApplication {
+    static void main(String[] args) {
+        SpringApplication.run TodoListApplication, args
+    }
+}
